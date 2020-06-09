566b1be9570d1527071c009fe4293c38e5e24cab
Justin Fu <justinjfu@gmail.com>
2020-06-08 17:19:59 -0700

    move files up

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from setuptools import find_packages
 setup(
     name='adept_envs',
     version='0.1',
-    package_data={
-      'adept_envs': ['adept_envs/franka/assets/*.xml'],
-    },
-    include_package_data=True,
+    #package_data={
+    #  'adept_envs': ['adept_envs/adept_envs/franka/assets/*.xml'],
+    #},
+    #include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     install_requires=['gym', 
                       'numpy', 
                       'mujoco_py', 
@@ -16,3 +18,4 @@ setup(
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
 )
+
